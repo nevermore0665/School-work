f597b3fe8aac33af1ef42467a3de3c001535bcbb
nevermore0665 <nevermore0665@gmail.com>
2025-02-03 14:46:31 -0600

    Initial commit

---
+++ MyFirstProgram.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int Year = 490;
+    float GravityEath = 8.91;
+    double GravitationalC = 6.67430e-8;
+    char RandomLetter = 'S';
+    bool Greekvictory = true;
+    string HistoryLesson = " The greeks won the battle of Marathon.";
+
+    cout << "Integer: "<< Year << endl;
+    cout << "Float: " << GravityEath << " m/s^2" << endl;
+    cout << "Double: " << GravitationalC  << endl;
+    cout << "Character: " << RandomLetter << endl;
+    cout << "Boolean: " << Greekvictory << endl;
+    cout << "String: " << HistoryLesson << endl;
+
+return 0;
+}
